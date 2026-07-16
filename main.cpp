3e3b4384547ea40825a6677ea7c6ca748c77bcac
ibraistara <ibraarifa398@gmail.com>
2026-07-16 11:45:42 +0700

    file open & reading riff header

--- main.cpp
+++ main.cpp
@@ -5,5 +5,25 @@
 #include <cstring>
 
 int main(){
+    std::string inputPath {"../input.wav"};
+    std::string outputPath {"../output.wav"};
+
+    // open wav file
+    std::ifstream inputFile(inputPath, std::ios::binary);
+    if (!inputFile.is_open()){
+        std::cerr << "Could not open file!\n";
+        return 1;
+    }
+    
+    // RIFF chunk extraction
+    char chunkId[4] {0};
+    inputFile.read(chunkId, 4);
+
+    uint32_t chunkSize;
+    inputFile.read(reinterpret_cast<char*>(&chunkSize), 4);
+
+    char format[4] {0};
+    inputFile.read(format, 4);
+
     return 0;
 }
